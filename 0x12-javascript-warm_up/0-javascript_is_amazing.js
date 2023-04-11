52e70a1926ed5063ba27c9daa4e262e8f2121718
Ambyzhale <ambroseoyar@gmail.com>
2023-04-11 16:54:53 +0000

    0-javascript is amazing

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-/*variable declaration*/
-const myVar = "JavaScript is amazing"
-/* printing the result*/
-console.log(myVar)
+
+/* variable declaration */
+
+const myVar = "JavaScript is amazing";
+
+/* printing the result */
+
+console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-/* variable declarations using var*/
-var a = "C is fun", b = "Python is cool", c = "JavaScript is amazing";
-/*printing the outputs*/
-console.log(a)
-console.log(b)
-console.log(c)
+/* variable declarations using var */
+const a = 'C is fun'; const b = 'Python is cool'; const c = 'JavaScript is amazing';
+/* printing the outputs */
+console.log(a);
+console.log(b);
+console.log(c);
