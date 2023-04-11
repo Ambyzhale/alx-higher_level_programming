3f5ed90cd8dd33996046521c410c584f5bf52c6e
Ambyzhale <ambroseoyar@gmail.com>
2023-04-11 21:29:53 +0000

    multi languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-/* variable declarations using var */
-const a = 'C is fun'; const b = 'Python is cool'; const c = 'JavaScript is amazing';
-/* printing the outputs */
-console.log(a);
-console.log(b);
-console.log(c);
+console.log('C is fun\nPython is cool\nJavascript is amazing');
