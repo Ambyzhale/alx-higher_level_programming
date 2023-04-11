5e023b530dbd6b95c544175db29667c4be0550ee
Ambyzhale <ambroseoyar@gmail.com>
2023-04-11 16:09:04 +0000

    printing multi-languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* variable declarations using var*/
+var a = "C is fun", b = "Python is cool", c = "JavaScript is amazing";
+/*printing the outputs*/
+console.log(a)
+console.log(b)
+console.log(c)
